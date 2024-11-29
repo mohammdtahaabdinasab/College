dd061d0155c4eae7c69157084e8dc6153d6525ae
Taha <mohammdtahaabdinasab@gmail.com>
2024-11-29 23:50:53 +0330

    CodeEditor

---
+++ Advanced Programing/1/CodeEditor/CodeEditor/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace CodeEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.header = new System.Windows.Forms.Panel();
+            this.BtnItalic = new System.Windows.Forms.Button();
+            this.BtnBold = new System.Windows.Forms.Button();
+            this.BtnOptions = new System.Windows.Forms.Button();
+            this.BtnMinimize = new System.Windows.Forms.Button();
+            this.BtnFullScreen = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.LblFileName = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.header.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // header
+            // 
+            this.header.BackColor = System.Drawing.Color.DimGray;
+            this.header.Controls.Add(this.BtnItalic);
+            this.header.Controls.Add(this.BtnBold);
+            this.header.Controls.Add(this.BtnOptions);
+            this.header.Controls.Add(this.BtnMinimize);
+            this.header.Controls.Add(this.BtnFullScreen);
+            this.header.Controls.Add(this.BtnClose);
+            this.header.Controls.Add(this.LblFileName);
+            this.header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.header.Location = new System.Drawing.Point(0, 0);
+            this.header.Margin = new System.Windows.Forms.Padding(0);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(900, 41);
+            this.header.TabIndex = 0;
+            this.header.Paint += new System.Windows.Forms.PaintEventHandler(this.header_Paint);
+            this.header.MouseDown += new System.Windows.Forms.MouseEventHandler(this.header_MouseDown);
+            this.header.MouseMove += new System.Windows.Forms.MouseEventHandler(this.header_MouseMove);
+            this.header.MouseUp += new System.Windows.Forms.MouseEventHandler(this.header_MouseUp);
+            // 
+            // BtnItalic
+            // 
+            this.BtnItalic.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BtnItalic.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnItalic.ForeColor = System.Drawing.Color.White;
+            this.BtnItalic.Location = new System.Drawing.Point(650, 0);
+            this.BtnItalic.Name = "BtnItalic";
+            this.BtnItalic.Size = new System.Drawing.Size(50, 41);
+            this.BtnItalic.TabIndex = 6;
+            this.BtnItalic.Text = "I";
+            this.BtnItalic.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnItalic.UseVisualStyleBackColor = true;
+            this.BtnItalic.Click += new System.EventHandler(this.BtnItalic_Click);
+            // 
+            // BtnBold
+            // 
+            this.BtnBold.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BtnBold.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnBold.ForeColor = System.Drawing.Color.White;
+            this.BtnBold.Location = new System.Drawing.Point(700, 0);
+            this.BtnBold.Name = "BtnBold";
+            this.BtnBold.Size = new System.Drawing.Size(50, 41);
+            this.BtnBold.TabIndex = 5;
+            this.BtnBold.Text = "B";
+            this.BtnBold.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnBold.UseVisualStyleBackColor = true;
+            this.BtnBold.Click += new System.EventHandler(this.BtnBold_Click);
+            // 
+            // BtnOptions
+            // 
+            this.BtnOptions.Dock = System.Windows.Forms.DockStyle.Left;
+            this.BtnOptions.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnOptions.ForeColor = System.Drawing.Color.White;
+            this.BtnOptions.Location = new System.Drawing.Point(0, 0);
+            this.BtnOptions.Name = "BtnOptions";
+            this.BtnOptions.Size = new System.Drawing.Size(50, 41);
+            this.BtnOptions.TabIndex = 4;
+            this.BtnOptions.Text = "/|\\";
+            this.BtnOptions.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnOptions.UseVisualStyleBackColor = true;
+            this.BtnOptions.Click += new System.EventHandler(this.BtnOptions_Click);
+            // 
+            // BtnMinimize
+            // 
+            this.BtnMinimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BtnMinimize.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnMinimize.ForeColor = System.Drawing.Color.White;
+            this.BtnMinimize.Location = new System.Drawing.Point(750, 0);
+            this.BtnMinimize.Name = "BtnMinimize";
+            this.BtnMinimize.Size = new System.Drawing.Size(50, 41);
+            this.BtnMinimize.TabIndex = 3;
+            this.BtnMinimize.Text = "-";
+            this.BtnMinimize.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnMinimize.UseVisualStyleBackColor = true;
+            this.BtnMinimize.Click += new System.EventHandler(this.BtnMinimize_Click);
+            // 
+            // BtnFullScreen
+            // 
+            this.BtnFullScreen.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BtnFullScreen.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnFullScreen.ForeColor = System.Drawing.Color.White;
+            this.BtnFullScreen.Location = new System.Drawing.Point(800, 0);
+            this.BtnFullScreen.Name = "BtnFullScreen";
+            this.BtnFullScreen.Size = new System.Drawing.Size(50, 41);
+            this.BtnFullScreen.TabIndex = 2;
+            this.BtnFullScreen.Text = "(=)";
+            this.BtnFullScreen.UseVisualStyleBackColor = true;
+            this.BtnFullScreen.Click += new System.EventHandler(this.BtnFullScreen_Click);
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BtnClose.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnClose.ForeColor = System.Drawing.Color.White;
+            this.BtnClose.Location = new System.Drawing.Point(850, 0);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(50, 41);
+            this.BtnClose.TabIndex = 1;
+            this.BtnClose.Text = "X";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // LblFileName
+            // 
+            this.LblFileName.AutoSize = true;
+            this.LblFileName.ForeColor = System.Drawing.Color.White;
+            this.LblFileName.Location = new System.Drawing.Point(56, 15);
+            this.LblFileName.Name = "LblFileName";
+            this.LblFileName.Size = new System.Drawing.Size(57, 15);
+            this.LblFileName.TabIndex = 0;
+            this.LblFileName.Text = "FileName";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 559);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(900, 559);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(900, 600);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.header);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MdiChildrenMinimizedAnchorBottom = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.header.ResumeLayout(false);
+            this.header.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel header;
+        private Button BtnMinimize;
+        private Button BtnFullScreen;
+        private Button BtnClose;
+        private Label LblFileName;
+        private Panel panel1;
+        private RichTextBox richTextBox1;
+        private Button BtnOptions;
+        private Button BtnItalic;
+        private Button BtnBold;
+    }
+}
---
+++ Advanced Programing/1/CodeEditor/CodeEditor/Form1.cs
@@ -0,0 +1,155 @@
+﻿namespace CodeEditor
+{
+    public partial class Form1 : Form
+    {
+        private bool isDragging = false;
+        bool saveFile = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+        string[] tags = {"html", "head", "body", "title", "style", "script", "div", "p", "h1", "h2", "h3", "h4", "h5", "h6","a"};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            panel1.BackColor = ColorTranslator.FromHtml("#23272e");
+            richTextBox1.AcceptsTab = true;
+        }
+
+        private void header_Paint(object sender, PaintEventArgs e)
+        {
+            header.BackColor = ColorTranslator.FromHtml("#2c313c");
+            BtnClose.BackColor = ColorTranslator.FromHtml("#23272e");
+            BtnFullScreen.BackColor = ColorTranslator.FromHtml("#23272e");
+            BtnMinimize.BackColor = ColorTranslator.FromHtml("#23272e");
+            BtnItalic.BackColor = ColorTranslator.FromHtml("#23272e");
+            BtnBold.BackColor = ColorTranslator.FromHtml("#23272e");
+            BtnOptions.BackColor = ColorTranslator.FromHtml("#23272e");
+            richTextBox1.BackColor = ColorTranslator.FromHtml("#23272e");
+        }
+
+        private void BtnFullScreen_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+            {
+                WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void BtnMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void header_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                if (WindowState == FormWindowState.Maximized)
+                {
+                    WindowState = FormWindowState.Normal;
+                }
+
+                Point cursorPoint = Cursor.Position;
+                this.Location = new Point(dragFormPoint.X - dragCursorPoint.X + cursorPoint.X, dragFormPoint.Y - dragCursorPoint.Y + cursorPoint.Y);
+            }
+        }
+
+        private void header_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                dragCursorPoint = Cursor.Position;
+                dragFormPoint = this.Location;
+            }
+        }
+
+        private void header_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string text = richTextBox1.Text;
+                int caretIndex = richTextBox1.SelectionStart;
+
+                if (text[caretIndex - 1] == '(')
+                {
+                    richTextBox1.Text = text.Insert(caretIndex, ")");
+                    richTextBox1.SelectionStart = caretIndex;
+                }
+                else if (text[caretIndex - 1] == '[')
+                {
+                    richTextBox1.Text = text.Insert(caretIndex, "]");
+                    richTextBox1.SelectionStart = caretIndex;
+                }
+                else if (text[caretIndex - 1] == '{')
+                {
+                    richTextBox1.Text = text.Insert(caretIndex, "}");
+                    richTextBox1.SelectionStart = caretIndex;
+                }
+
+                for (int i = 0; i <= tags.Length; i++)
+                {
+                    if (text.Substring(caretIndex - (tags[i].Length+1), (tags[i].Length+1)) == "<"+tags[i])
+                    {
+                        richTextBox1.Text = text.Insert(caretIndex, "></"+tags[i]+">");
+                        richTextBox1.SelectionStart = caretIndex + 1;
+                    }
+
+                }
+                saveFile = false;
+            }
+            catch (Exception ex) { }
+        }
+
+        private void BtnOptions_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "HTML Files (*.html)|*.html";
+                openFileDialog.Title = "ذخیره فایل";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    string fileContent = richTextBox1.Text;
+                    File.WriteAllText(filePath, fileContent);
+                    LblFileName.Text = filePath;
+                }
+            }
+            saveFile = true;
+        }
+
+        private void BtnItalic_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, FontStyle.Italic);
+            }
+        }
+
+        private void BtnBold_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, FontStyle.Bold);
+            }
+
+        }
+    }
+}
